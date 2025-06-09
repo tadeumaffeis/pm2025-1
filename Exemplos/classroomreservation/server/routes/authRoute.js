25a45330d27499d756fa0935861d74c6ce282ba4
Antonio Tadeu Maffeis <tadeu.maffeis@gmail.com>
2025-06-09 14:49:17 -0300

    Alterações 09-06-2025-14-50-00

--- Exemplos/classroomreservation/server/routes/authRoute.js
+++ Exemplos/classroomreservation/server/routes/authRoute.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const {
     loginAuth,
-    verifyAuth
+    verifyAuth,
+    checkAuth
 } = require('../controller/authController');
 
 // Listar todos os cursos
 router.post('/login', loginAuth);
 
 // Inserir um novo curso na view
-router.post('/verify', verifyAuth);
+router.post('/verify', checkAuth, verifyAuth);
 
 module.exports = router;
